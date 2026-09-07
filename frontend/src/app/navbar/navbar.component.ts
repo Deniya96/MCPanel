be3f7da01b3c8d8f8125601a3d6285a043db1dc9
deniya96 <deniyaplayer@gmail.com>
2026-09-07 09:33:02 +0300

    update title and simplify navbar component

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
-import {NavigationEnd, Router, RouterLink, RouterLinkActive} from "@angular/router";
-import {filter} from "rxjs";
-import {NgIf} from "@angular/common";
+import { RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -9,19 +7,9 @@ import {NgIf} from "@angular/common";
   imports: [
     RouterLink,
     RouterLinkActive,
-    NgIf
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  isHidden = false;
-
-  constructor(private router: Router) {
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: any) => {
-        this.isHidden = event.url.includes('/poshalko');
-      });
-  }
 }
