81de6fd0da116165027858c18a7cf6cdcb05a091
deniya96 <deniyaplayer@gmail.com>
2026-09-07 09:40:26 +0300

    removed tips

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import {provideRouter, Routes, withRouterConfig } from '@angular/router';
 import {ConsoleComponent} from "./console/console.component";
-import {TipsComponent} from "./tips/tips.component";
 import {SettingsComponent} from "./settings/settings.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'console', pathMatch: 'full' },
   { path: 'console', component: ConsoleComponent},
-  { path: 'tips', component: TipsComponent},
   { path: 'settings', component: SettingsComponent},
 ];
 export const appRouting = provideRouter(routes,
